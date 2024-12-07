b03bf9a741cd50d8fe0501bc72d8405679385fcd
FWalkowicz <fillio00@wp.pl>
2024-12-07 21:45:30 +0100

    basic matrix operations

--- main.c
+++ main.c
@@ -43,6 +43,52 @@ void displayMatrix(Matrix mat) {
     printf("]\n");
 }
 
+void dotProduct(Matrix mat_a, Matrix mat_b, Matrix result) {
+    result.rows = mat_a.rows;
+    result.cols = mat_b.cols;
+    for (int i = 0; i < result.rows; i++) {
+        for (int j = 0; j < result.cols; j++) {
+            for (int k = 0; k < mat_a.cols; k++) {
+                result.data[i * result.cols + j] += mat_a.data[i * result.cols + k] * mat_b.data[k * result.cols + j];
+            }
+        }
+    }
+}
+
+void addMatrix(Matrix result, Matrix mat) {
+    for (int i = 0; i < result.rows; i++) {
+        for (int j = 0; j < result.cols; j++) {
+            result.data[i * result.cols + j] += mat.data[i * result.cols + j];
+        }
+    }
+}
+
+void subtractMatrix(Matrix result, Matrix mat) {
+    for (int i = 0; i < result.rows; i++) {
+        for (int j = 0; j < result.cols; j++) {
+            result.data[i * result.cols + j] -= mat.data[i * result.cols + j];
+        }
+    }
+}
+
+Matrix transposeMatrix(Matrix mat) {
+    Matrix result = createMatrix(mat.cols, mat.rows);
+    for (int i = 0; i < result.rows; i++) {
+        for (int j = 0; j < result.cols; j++) {
+            result.data[i * result.cols + j] = mat.data[j * mat.cols + i];
+        }
+    }
+    return result;
+}
+
+void multipleMatrixByValue(Matrix result, float value) {
+    for (int i = 0; i < result.rows; i++) {
+        for (int j = 0; j < result.cols; j++) {
+            result.data[i * result.cols + j] *= value;
+        }
+    }
+}
+
 int main(void) {
     Matrix mat = createMatrix(3, 3);
     initMatrix(mat);
